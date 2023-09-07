4c575a9924c0339cf45a371cf8fe40b3739c130b
Ubuntu <ubuntu@ip-172-31-29-249.eu-west-1.compute.internal>
2023-09-07 14:15:45 +0000

    folder structure

--- src/main/java/se/hiq/feedbaq/FeedbaqApplication.java
+++ src/main/java/se/hiq/feedbaq/FeedbaqApplication.java
@@ -2,10 +2,7 @@ package se.hiq.feedbaq;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
 
-@Controller
 @SpringBootApplication
 public class FeedbaqApplication {
 	
