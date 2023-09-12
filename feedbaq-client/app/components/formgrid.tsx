b26306b3fa1cf4d433d925694067868b33abfb51
Ubuntu <ubuntu@ip-172-31-31-168.eu-west-1.compute.internal>
2023-09-12 14:50:40 +0000

    fixed customer stepper

--- feedbaq-client/app/components/formgrid.tsx
+++ feedbaq-client/app/components/formgrid.tsx
@@ -1,10 +1,76 @@
 'use client';
 import { useState } from 'react';
-import { Grid, Stepper, Step, StepLabel, Button } from '@mui/material'; 
+import { Grid, Stepper, Step, StepLabel, Button, TextField } from '@mui/material'; 
 import QuestionTemplate from './questiontemplate.tsx';
 import '../styles/form.css';
 
+import StepConnector, { stepConnectorClasses } from '@mui/material/StepConnector';
+import { StepIconProps } from '@mui/material/StepIcon';
+import { styled } from '@mui/material/styles';
+import Check from '@mui/icons-material/Check';
+
+
 export default function FormGrid() {
+    
+    const QontoConnector = styled(StepConnector)(({ theme }) => ({
+      [`&.${stepConnectorClasses.alternativeLabel}`]: {
+        top: 10,
+        left: 'calc(-50% + 16px)',
+        right: 'calc(50% + 16px)',
+      },
+      [`&.${stepConnectorClasses.active}`]: {
+        [`& .${stepConnectorClasses.line}`]: {
+          borderColor: '#784af4',
+        },
+      },
+      [`&.${stepConnectorClasses.completed}`]: {
+        [`& .${stepConnectorClasses.line}`]: {
+          borderColor: '#784af4',
+        },
+      },
+      [`& .${stepConnectorClasses.line}`]: {
+        borderColor: theme.palette.mode === 'dark' ? theme.palette.grey[800] : '#eaeaf0',
+        borderTopWidth: 3,
+        borderRadius: 1,
+      },
+    }));
+    
+    const QontoStepIconRoot = styled('div')<{ ownerState: { active?: boolean } }>(
+      ({ theme, ownerState }) => ({
+        color: theme.palette.mode === 'dark' ? theme.palette.grey[700] : '#eaeaf0',
+        display: 'flex',
+        height: 22,
+        alignItems: 'center',
+        ...(ownerState.active && {
+          color: '#784af4',
+        }),
+        '& .QontoStepIcon-completedIcon': {
+          color: '#784af4',
+          zIndex: 1,
+          fontSize: 18,
+        },
+        '& .QontoStepIcon-circle': {
+          width: 8,
+          height: 8,
+          borderRadius: '50%',
+          backgroundColor: 'currentColor',
+        },
+      }),
+    );
+    
+    function QontoStepIcon(props: StepIconProps) {
+      const { active, completed, className } = props;
+    
+      return (
+        <QontoStepIconRoot ownerState={{ active }} className={className}>
+          {completed ? (
+            <Check className="QontoStepIcon-completedIcon" />
+          ) : (
+            <div className="QontoStepIcon-circle" />
+          )}
+        </QontoStepIconRoot>
+      );
+    }
     
     const [activeStep, setActiveStep] = useState(0);
 
@@ -36,6 +102,7 @@ export default function FormGrid() {
         }
     ];
     
+    const [inputValues, setInputValues] = useState(Array(questions.length).fill(''));
     
     
     const handleNext = () => {
@@ -44,6 +111,12 @@ export default function FormGrid() {
     
     const handleBack = () => {
         setActiveStep((prevActiveStep) => prevActiveStep - 1);
+    }
+    
+    const handleInputChange = (event: any) => {
+        const newInputValues = [...inputValues];
+        newInputValues[activeStep] = event.target.value;
+        setInputValues(newInputValues);
     }
     
     
@@ -68,8 +141,17 @@ export default function FormGrid() {
                     );
                 })}
             </Grid>
-            <Grid item xs={6} className="middleRow centerContent">
+            <Grid item xs={6} sx={{flexDirection: 'column'}} className="middleRow centerContent">
                 <h1>{questions[activeStep].question}</h1>
+                <TextField
+                  value={inputValues[activeStep]}
+                  onChange={handleInputChange}
+                  placeholder="Skriv ditt svar här ..."
+                  multiline
+                  fullWidth
+                  rows={4}
+                  variant="outlined"
+                />
             </Grid>
             <Grid item xs={3} className="middleRow centerContent">
                 <h3>{questions[activeStep].description}</h3>
@@ -80,11 +162,11 @@ export default function FormGrid() {
                 <Button variant="contained" onClick={handleBack}>Back</Button>
             </Grid>
             <Grid item xs={4} className="bottomRow centerContent">
-                <Stepper activeStep={activeStep}>
+                <Stepper alternativeLabel activeStep={activeStep} connector={<QontoConnector/>}>
                     {questions.map((label, index) => {
                         return (
                             <Step key={index}>
-                                <StepLabel>{index + 1}</StepLabel>
+                                <StepLabel StepIconComponent={QontoStepIcon}/>
                             </Step>
                         );
                     })}
