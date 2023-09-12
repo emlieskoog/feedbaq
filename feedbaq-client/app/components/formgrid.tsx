cfe6ebec5375458cb055dbd7295d8a340456fb10
Ubuntu <ubuntu@ip-172-31-29-249.eu-west-1.compute.internal>
2023-09-12 12:51:37 +0000

    continued work on the form page

--- feedbaq-client/app/components/formgrid.tsx
+++ feedbaq-client/app/components/formgrid.tsx
@@ -1,42 +1,97 @@
-import { Grid } from '@mui/material'; 
+'use client';
+import { useState } from 'react';
+import { Grid, Stepper, Step, StepLabel, Button } from '@mui/material'; 
 import QuestionTemplate from './questiontemplate.tsx';
 import '../styles/form.css';
 
 export default function FormGrid() {
     
+    const [activeStep, setActiveStep] = useState(0);
+
+    const questions = [
+        {
+            question: 'Fråga 1',
+            description: 'Beskrivning 1',
+            inputType: '...'
+        },
+        {
+            question: 'Fråga 2',
+            description: 'Beskrivning 2',
+            inputType: '...'
+        },
+        {
+            question: 'Fråga 3',
+            description: 'Beskrivning 3',
+            inputType: '...'
+        },
+        {
+            question: 'Fråga 4',
+            description: 'Beskrivning 4',
+            inputType: '...'
+        },
+        {
+            question: 'Fråga 5',
+            description: 'Beskrivning 5',
+            inputType: '...'
+        }
+    ];
+    
+    
+    
+    const handleNext = () => {
+        setActiveStep((prevActiveStep) => prevActiveStep + 1);
+    }
+    
+    const handleBack = () => {
+        setActiveStep((prevActiveStep) => prevActiveStep - 1);
+    }
+    
+    
     return (
         <Grid container spacing={2} className="outerGrid">
             {/* First row */}
-            <Grid item xs={4} className="topRow centerContent">
-                <h1>Test</h1>
-            </Grid>
-            <Grid item xs={4} className="topRow centerContent">
-                <h1>Test 2</h1>
-            </Grid>
-            <Grid item xs={4} className="topRow centerContent">
-                <h1>Test 3</h1>
+            <Grid item xs={12} className="topRow centerContent">
+                <h1>Kvalitetsuppföljning</h1>
             </Grid>
             
             {/* Second row */}
-            <Grid item xs={3} className="middleRow centerContent">
-                <h1>Test 4</h1>
+            <Grid item xs={3} sx={{flexDirection: 'column'}} className="middleRow centerContent">
+                {questions.map((q, index) => {
+                    return (
+                        <div key={index}>
+                          {index === activeStep ? (
+                            <h5>{q.question}</h5>
+                          ) : (
+                            <h6>{q.question}</h6>
+                          )}
+                        </div>
+                    );
+                })}
             </Grid>
             <Grid item xs={6} className="middleRow centerContent">
-                <QuestionTemplate/>
+                <h1>{questions[activeStep].question}</h1>
             </Grid>
             <Grid item xs={3} className="middleRow centerContent">
-                <h1>Test 6</h1>
+                <h3>{questions[activeStep].description}</h3>
             </Grid>
             
             {/* Third row */}
             <Grid item xs={4} className="bottomRow centerContent">
-                <h1>Test 7</h1>
+                <Button variant="contained" onClick={handleBack}>Back</Button>
             </Grid>
             <Grid item xs={4} className="bottomRow centerContent">
-                <h1>Test 8</h1>
+                <Stepper activeStep={activeStep}>
+                    {questions.map((label, index) => {
+                        return (
+                            <Step key={index}>
+                                <StepLabel>{index + 1}</StepLabel>
+                            </Step>
+                        );
+                    })}
+                </Stepper>
             </Grid>
             <Grid item xs={4} className="bottomRow centerContent">
-                <h1>Test 9</h1>
+                <Button variant="contained" onClick={handleNext}>Next</Button>
             </Grid>
         </Grid>
     );
