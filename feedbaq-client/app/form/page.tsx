b47e11eb618a7309e601e31c7e4c1935cbca065f
Wilma <wilma.axelsson@hiq.se>
2023-09-14 16:36:13 +0200

    added more styling to view form page

--- feedbaq-client/app/form/page.tsx
+++ feedbaq-client/app/form/page.tsx
@@ -1,15 +1,55 @@
+import { Block } from '@mui/icons-material';
 import { Box } from '@mui/material';
+import '../styles/form.css';
 
 export default function FormDetail() {
-    return(
-        <Box sx={{height:'100vh', minWidth:'100%'}}>
-            <Box sx={{flexDirection:'row', display:'flex', margin:'20px 300px', justifyContent: 'space-evenly'}} > 
-                <h2> Kund </h2>
-                <h2> Konsult </h2>
-                <h2> Datum </h2>
+
+    const qualityFollowUp = [
+        {
+            customer: "Ica",
+            consultant: "Agnes",
+            date: "2023-09-13",
+            text1: 'Konsulten var jätteglad',
+            text2: 'Konsulten hade jättekul'
+        },
+    ];
+
+    return (
+        <Box sx={{ height: '100vh', minWidth: '100%' }}>
+            <Box sx={{ flexDirection: 'row', display: 'flex', margin: '15px 300px', justifyContent: 'space-evenly'}} >
+                <h2> Ica </h2>
+                <h2> Agnes </h2>
+                <h2> 2023-09-13 </h2>
             </Box>
-            <h3> Glädje </h3>
-            <p> Var du glad? </p>
+            <div className='centerContent'>
+                <Box sx={{
+                    display: 'block',
+                    backgroundColor: 'white',
+                    border: 1,
+                    borderRadius: 2,
+                    borderColor: 'lightgrey',
+                    m: 1,
+                    p: 2,
+                    width: '70%'
+                }}>
+                    {qualityFollowUp.map((followUp, index) => {
+                        return (
+                            <div>
+                                <h3> Kund </h3>
+                                <p className='separator'> {followUp.customer} </p>
+                                <h3> Konsult </h3>
+                                <p className='separator'> {followUp.consultant} </p>
+                                <h3> Datum </h3>
+                                <p className='separator'> {followUp.date} </p>
+                                <h3> Glädje? </h3>
+                                <p className='separator'> {followUp.text1} </p>
+                                <h3> Kul? </h3>
+                                <p> {followUp.text2} </p>
+                            </div>
+                        );
+                    })}
+                </Box>
+            </div>
         </Box>
     );
 }
