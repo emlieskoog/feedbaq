315ee1b5eccf9c142a52f87b74c2e4e99ba134fc
Emelie Skoog <emelie.skoog@hiq.se>
2023-09-19 11:07:24 +0200

    add error handling getForms

--- server/src/main/java/se/hiq/feedbaq/FormController.java
+++ server/src/main/java/se/hiq/feedbaq/FormController.java
@@ -2,11 +2,12 @@ package se.hiq.feedbaq;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
 
 import java.util.List;
 import java.util.Map;
@@ -21,8 +22,13 @@ public class FormController {
     
     @GetMapping("/forms")
     public ResponseEntity<Object> getForms() {
-        List<Map<String, Object>> res = jdbcTemplate.queryForList("SELECT * FROM forms;");
-        return new ResponseEntity<>(res, HttpStatus.OK);
+        try{
+            List<Map<String, Object>> res = jdbcTemplate.queryForList("SELECT * FROM forms;");
+            return new ResponseEntity<>(res, HttpStatus.OK);
+        } catch (DataAccessException e) {
+            String errorMessage = "An error occurred while fetching forms: " + e.getMessage();
+            return new ResponseEntity<>(errorMessage, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
     
 }
