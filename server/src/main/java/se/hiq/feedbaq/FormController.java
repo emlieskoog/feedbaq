4b44111383918c4c33c8af41ab5dcb0f38e90484
Ubuntu <ubuntu@ip-172-31-29-249.eu-west-1.compute.internal>
2023-09-19 14:19:50 +0000

    implemented function and unit tests for retrieving a form by a specific id

--- server/src/main/java/se/hiq/feedbaq/FormController.java
+++ server/src/main/java/se/hiq/feedbaq/FormController.java
@@ -1,13 +1,16 @@
 package se.hiq.feedbaq;
 
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
+import org.springframework.dao.IncorrectResultSizeDataAccessException;
 
 import java.util.List;
 import java.util.Map;
@@ -23,10 +26,26 @@ public class FormController {
     @GetMapping("/forms")
     public ResponseEntity<Object> getForms() {
         try {
-            List<Map<String, Object>> res = jdbcTemplate.queryForList("SELECT * FROM forms;");
-            return new ResponseEntity<>(res, HttpStatus.OK);
+            List<Map<String, Object>> result = jdbcTemplate.queryForList("SELECT * FROM forms;");
+            return new ResponseEntity<>(result, HttpStatus.OK);
         } catch (DataAccessException e) {
             String errorMessage = "An error occurred while fetching forms: " + e.getMessage();
+            return new ResponseEntity<>(errorMessage, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+    
+    
+    @GetMapping("/forms/{id}")
+    public ResponseEntity<Object> getFormById(@PathVariable int id) {
+        try {
+            String query = "SELECT * FROM forms WHERE id=?";
+            Map<String, Object> result = jdbcTemplate.queryForMap(query, id);
+            return new ResponseEntity<>(result, HttpStatus.OK);
+        } catch (IncorrectResultSizeDataAccessException e) {
+            String errorMessage = "Form with ID " + id + " not found.";
+            return new ResponseEntity<>(errorMessage, HttpStatus.NOT_FOUND);
+        } catch (DataAccessException e) {
+            String errorMessage = "An error occured while fetching form with ID " + id + ": " + e.getMessage();
             return new ResponseEntity<>(errorMessage, HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
--- server/src/test/java/se/hiq/feedbaq/unit/FormControllerTests.java
+++ server/src/test/java/se/hiq/feedbaq/unit/FormControllerTests.java
@@ -11,6 +11,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.dao.DataAccessException;
+import org.springframework.dao.IncorrectResultSizeDataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -64,6 +65,63 @@ public class FormControllerTests {
         // Assert
         assertEquals(HttpStatus.INTERNAL_SERVER_ERROR, response.getStatusCode());
         assertEquals("An error occurred while fetching forms: Test DataAccessException", response.getBody());
+    }
+    
+    @Test
+    public void testGetFormByIdSuccess() {
+        
+        // Create mock data
+        Map<String, Object> mockData = new HashMap<String, Object>();
+        mockData.put("id", 1);
+        mockData.put("name", "form1");
+        
+        // Mock the behavior of jdbcTemplate.queryForMap to return mock data above
+        String query = "SELECT * FROM forms WHERE id=?";
+        int id = 1;
+        when(jdbcTemplate.queryForMap(query, id)).thenReturn(mockData);
+        
+        // Act
+        ResponseEntity<Object> response = formController.getFormById(id);
+
+        // Assert        
+        assertNotNull(response);
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertEquals(mockData, response.getBody());
+    }
+    
+    @Test
+    public void testGetFormByIdNotFound() {
+        
+        // Mock the behavior of jdbcTemplate.queryForMap to throw a IncorrectResultSizeDataAccessException
+        String query = "SELECT * FROM forms WHERE id=?";
+        int id = 1;
+        IncorrectResultSizeDataAccessException exception = new IncorrectResultSizeDataAccessException("Form with ID " + id + " not found.", id);
+        doThrow(exception).when(jdbcTemplate).queryForMap(query, id);
+        
+        // Act
+        ResponseEntity<Object> response = formController.getFormById(id);
+        
+        // Assert
+        assertEquals(HttpStatus.NOT_FOUND, response.getStatusCode());
+        assertEquals("Form with ID " + id + " not found.", response.getBody());
+        
+    }
+    
+    @Test
+    public void testGetFormByIdThrowsDataAccessException() {
+        
+        // Mock the behavior of jdbcTemplate.queryForMap to throw a DataAccessException
+        String query = "SELECT * FROM forms WHERE id=?";
+        int id = 1;
+        doThrow(new DataAccessException("Test DataAccessException") {}).when(jdbcTemplate).queryForMap(query, id);
+
+        // Act
+        ResponseEntity<Object> response = formController.getFormById(id);
+        
+        // Assert
+        assertEquals(HttpStatus.INTERNAL_SERVER_ERROR, response.getStatusCode());
+        assertEquals("An error occured while fetching form with ID " + id + ": Test DataAccessException", response.getBody());
+        
     }
     
     
