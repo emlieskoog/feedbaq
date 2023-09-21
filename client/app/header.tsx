c5f18ce3f475e9897a71f2e7e5ee87760df85303
Wilma <wilma.axelsson@hiq.se>
2023-09-21 16:50:38 +0200

    fix logo image to show on all pages

--- client/app/header.tsx
+++ client/app/header.tsx
@@ -1,11 +1,11 @@
-import * as React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
-import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
 
 export default function Header() {
   return (
@@ -21,11 +21,17 @@ export default function Header() {
           >
             <MenuIcon />
           </IconButton>
-          <img src='HiQ_logo_white.png' alt='HiQ Logo' style={{height: '35px', marginRight: '15px'}}></img>
+          <img
+            src="/HiQ_logo_white.png"
+            alt="HiQ Logo"
+            style={{ height: "35px", marginRight: "15px" }}
+          ></img>
           <Typography variant="h5" component="div" sx={{ flexGrow: 1 }}>
             Kvalitetsuppföljning
           </Typography>
-          <Button color="inherit" href="/">Logga ut</Button>
+          <Button color="inherit" href="/">
+            Logga ut
+          </Button>
         </Toolbar>
       </AppBar>
     </Box>
