1ca0d24f93a4db3346018ce762735c857636c4b0
Lucas Hultqvist <lucas.hultqvist@hiq.se>
2023-09-29 13:21:24 +0200

    fixes

--- client/app/account/formdetails/page.tsx
+++ client/app/account/formdetails/page.tsx
@@ -16,8 +16,6 @@ export default function FormDetails() {
   useEffect(() => {
     setIsLoading(true);
     fetchFormData();
-
-    setIsLoading(false);
   }, []);
 
   async function fetchFormData() {
@@ -29,11 +27,11 @@ export default function FormDetails() {
       const responseFormData = await fetch(
         "http://localhost:8080/api/forms/" + id
       );
-      const formData = await responseFormData.json();
-      setFormData(formData);
+      const data = await responseFormData.json();
+      setFormData(data);
+      setIsLoading(false);
 
-      console.log(formData);
-
+      /*
       const responseDetailedFormData = await fetch(
         "http://localhost:8080/api/form_responses/" + id
       );
@@ -51,6 +49,7 @@ export default function FormDetails() {
       const namesData = await responseNamesData.json();
       console.log(namesData);
       setNamesData(namesData);
+      */
     } catch (error) {
       console.log("An error occured when trying to retrieve form data.", error);
     }
@@ -72,9 +71,9 @@ export default function FormDetails() {
               justifyContent: "space-evenly",
             }}
           >
-            <h2> {namesData.customerName} </h2>
-            <h2> {namesData.consultantName} </h2>
-            <h2> {formData.date} </h2>
+            <h2> {formData.customer_name} </h2>
+            <h2> {formData.consultant_name} </h2>
+            <h2> {formData.form_data.date} </h2>
           </Box>
           <div className="centerContent">
             <Box
@@ -91,31 +90,31 @@ export default function FormDetails() {
             >
               <div>
                 <h3> Kund </h3>
-                <p className="separator"> {namesData.customerName} </p>
+                <p className="separator"> {formData.customer_name} </p>
                 <h3> Konsult </h3>
-                <p className="separator"> {namesData.consultantName} </p>
+                <p className="separator"> {formData.consultant_name} </p>
                 <h3> Säljare </h3>
-                <p className="separator"> {namesData.salesName} </p>
+                <p className="separator"> {formData.sales_name} </p>
                 <h3> Datum </h3>
-                <p className="separator"> {formData.date} </p>
+                <p className="separator"> {formData.form_data.date} </p>
                 <h3> Uppstart? </h3>
-                <p className="separator"> {detailedFormData.q1} </p>
+                <p className="separator"> {formData.form_data.q1} </p>
                 <h3> Resultat? </h3>
-                <p className="separator"> {detailedFormData.q2}</p>
+                <p className="separator"> {formData.form_data.q2}</p>
                 <h3> Ansvar? </h3>
-                <p className="separator"> {detailedFormData.q3} </p>
+                <p className="separator"> {formData.form_data.q3} </p>
                 <h3> Enkelhet? </h3>
-                <p className="separator"> {detailedFormData.q4}</p>
+                <p className="separator"> {formData.form_data.q4}</p>
                 <h3> Glädje? </h3>
-                <p className="separator"> {detailedFormData.q5}</p>
+                <p className="separator"> {formData.form_data.q5}</p>
                 <h3> Innovation? </h3>
-                <p className="separator"> {detailedFormData.q6}</p>
+                <p className="separator"> {formData.form_data.q6}</p>
                 <h3> Nöjdhet konsult? </h3>
-                <p className="separator"> {detailedFormData.q7}</p>
+                <p className="separator"> {formData.form_data.q7}</p>
                 <h3> Nöjdhet HiQ? </h3>
-                <p className="separator"> {detailedFormData.q8}</p>
+                <p className="separator"> {formData.form_data.q8}</p>
                 <h3> Förbättringar? </h3>
-                <p className="separator"> {detailedFormData.q9}</p>
+                <p className="separator"> {formData.form_data.q9}</p>
                 <h3> Värdeomdömen? </h3>
                 <p> </p>
               </div>
