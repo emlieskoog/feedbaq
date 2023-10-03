4a3c3cdd6ddbf657963592ed9520f3b9555c4d94
Lucas Hultqvist <lucas.hultqvist@hiq.se>
2023-10-03 16:31:29 +0200

    test: quick test to try codebuild

--- server/src/test/java/se/hiq/feedbaq/controller/AuthControllerTests.java
+++ server/src/test/java/se/hiq/feedbaq/controller/AuthControllerTests.java
@@ -54,7 +54,7 @@ public class AuthControllerTests {
         requestBody.put("email", "test@hiq.se");
 
         // SQL query
-        String sql = "SELECT count(*) FROM users WHERE email=?";
+        String sql = "SELECT count(*) FROM users WHERE emaila=?";
 
         // Mock behavior of jdbcTemplate.queryForObject to return 1
         when(jdbcTemplate.queryForObject(sql, Integer.class, requestBody.get("email"))).thenReturn(1);
