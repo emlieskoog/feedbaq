f86db266d702111918ce21d90848661d957f28c2
Emelie Skoog <emelie.skoog@hiq.se>
2023-10-04 19:36:46 +0200

    move formquestions to constants

--- client/middleware.js
+++ client/middleware.js
@@ -5,7 +5,7 @@ export default createMiddleware({
     locales: ['en', 'sv'],
 
     // If this locale is matched, pathnames work without a prefix (e.g. `/about`)
-    defaultLocale: 'en'
+    defaultLocale: 'sv'
 });
 
 export const config = {
